2072befe2b85890b0507490fcd4010f1f6df9ff9
dem585 <65715072+dem585@users.noreply.github.com>
2021-06-26 14:54:43 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -37,3 +37,42 @@ $(function () {
       document.body.classList.remove('loaded_hiding');
     }, 500);
   }
+
+
+//animation
+  const animItems = document.querySelectorAll('._anim-items');
+
+  if(animItems.length > 0){
+    window.addEventListener('scroll' , animOnScroll);
+    function animOnScroll(){
+      for (let index = 0; index < animItems.length; index++) {
+       const animItem = animItems[index];
+       const animItemHeight = animItem.offsetHeight;
+       const animItemOffset = offser(animItem).top;
+       const animStart = 4;
+
+       let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+       if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - animItemHeight / animStart;
+       }
+
+       if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)){
+        animItem.classList.add('_active');
+       }else{
+          if (!animItem.classList.contains('_anim-no-hide')) {
+            // animItem.classList.remove('_active'); /*убрал повторную анимацию*/
+          }        
+       }
+      }
+      function offser(el){
+        const rect = el.getBoundingClientRect(),
+          scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+          scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}  
+      }
+    }
+    setTimeout(() =>{
+      animOnScroll();      
+    }, 300);    
+  }
